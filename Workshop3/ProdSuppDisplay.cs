3bb32b0705b387bf00d5b982c00121aecf2c9668
Kun Zhang <Kun.Zhang01@edu.sait.ca>
2019-05-28 10:12:13 -0600

    Partial work for product supplier display, no add/edit/delete functions

--- Workshop3/ProdSuppDisplay.cs
+++ Workshop3/ProdSuppDisplay.cs
@@ -8,11 +8,13 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using BusinessLayer;
+using DataLayer;
 
 namespace Workshop3
 {
     public partial class ProdSuppDisplay : Form
     {
+        int i = 0;
         public ProdSuppDisplay()
         {
             InitializeComponent();
@@ -20,38 +22,57 @@ namespace Workshop3
 
         private void ProdSuppDisplay_Load(object sender, EventArgs e)
         {
-            List<PkgProductsSuppliers> ps = PkgProductsSuppliers.GetPSList();            
-            //PSdataGridView.DataSource = ps.Select(x => new { x.ProductName, x.SupplierName }).ToList();
+            List<PkgProductsSuppliers> ps = PkgProductsSuppliers.GetPSList();
+            PSdataGridView.DataSource = ps.Select(x => new {x.ProductSupplierId, x.ProductID, x.ProductName, x.SupplierID, x.SupplierName }).ToList();
             PSdataGridView.Columns["SupplierName"].Width = 200;
-
+            PSdataGridView.Columns["ProductID"].Visible = false;
+            PSdataGridView.Columns["SupplierID"].Visible = false;
+            var ii = ps.OrderByDescending(p => p.ProductSupplierId).FirstOrDefault();
             List<Product> lp = Product.GetList();
-            prodBox.DataSource = lp;
+
             prodBox.DisplayMember = "ProdName";
-           // prodBox.ValueMember = "ProductId";
+            prodBox.ValueMember = "ProductId";
+            prodBox.DataSource = lp;
 
-           // List<Supplier> ls = Supplier.GetList();
-           //  suppBox.DataSource = ls;
+            List<PkgProductsSuppliers> lps = PkgProductsSuppliers.GetPSList();
+            PSCombo.DisplayMember = "ProductSupplierId";
+            PSCombo.ValueMember = "ProductSupplierId";
+            PSCombo.DataSource = lps;
+
+
+
         }
-
+      
+        private void PSCombo_SelectedValueChanged(object sender, EventArgs e)
+        {
+            int index = Convert.ToInt32(PSCombo.SelectedValue);
+            if (index == 0) index = 1;
+            List<PkgProductsSuppliers> lps = PkgProductsSuppliers.GetPSList();
+            List<PkgProductsSuppliers> ps = lps.Where(l => l.ProductSupplierId == index).ToList<PkgProductsSuppliers>();
+            this.prodBox.SelectedValue = ps[0].ProductName;
+            this.SuppBox.SelectedValue = ps[0].SupplierName;
+        }
 
         private void btnClose_Click(object sender, EventArgs e)
         {
             this.Close();
         }
 
-        private void btnClose_Click_1(object sender, EventArgs e)
-        {
-            this.Close();
-        }
-
         private void btnAdd_Click(object sender, EventArgs e)
         {
-
+             if ((prodBox.DisplayMember != "") && (SuppBox.DisplayMember != ""))
+                {
+                    PkgProductsSuppliersDB.Insert_Product_Supplier(Convert.ToInt32(prodBox.ValueMember), Convert.ToInt32(SuppBox.ValueMember));
+                }
         }
 
-        private void PSdataGridView_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        private void PSdataGridView_CellMouseClick(object sender, DataGridViewCellEventArgs e)
         {
-            label1.Text = PSdataGridView.Rows[e.RowIndex].Cells[0].ToString();
+            //  prodBox.DisplayMember = PSdataGridView.Rows[e.RowIndex].Cells["ProductName"].Value.ToString();
+          //  prodBox.ValueMember = PSdataGridView.Rows[e.RowIndex].Cells["ProductName"].Value.ToString();
+          //  SuppBox.DisplayMember = PSdataGridView.Rows[e.RowIndex].Cells["SupplierName"].Value.ToString();
         }
+        
     }
+
 }
